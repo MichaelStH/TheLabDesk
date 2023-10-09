8953bae80ebeb9be0ce6767ca8b5b8298e5c102e
MichaelStH <michael.sainthonore@gmail.com>
2023-10-09 11:14:00 +0200

    [develop]: add lab icon in toolbar

--- src/main/kotlin/ui/Main.kt
+++ src/main/kotlin/ui/Main.kt
@@ -2,6 +2,9 @@ package ui
 
 import TheLabDeskApp
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -12,8 +15,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import core.compose.component.AppTitleBar
+import core.compose.utils.WindowDraggableArea
 import core.log.Timber
 
 @Composable
@@ -33,15 +42,29 @@ fun App() {
 }
 
 fun main() = application {
-    
+
     // Init Timber Logging
     TheLabDeskApp.initArbor()
     Timber.d("main() | applicationScope")
 
-    Window(onCloseRequest = ::exitApplication) {
-        // Custom title toolbar
+    val windowState: WindowState = rememberWindowState(width = 850.dp, height = 600.dp)
 
-        // App Content
-        App()
+    Window(
+        state = windowState,
+        undecorated = true,
+        //transparent = true,
+        resizable = false,
+        onCloseRequest = ::exitApplication) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            // Custom title toolbar
+            WindowDraggableArea(modifier = Modifier.fillMaxWidth()) {
+                AppTitleBar(windowState) {
+                    exitApplication()
+                }
+            }
+
+            // App Content
+            App()
+        }
     }
 }
