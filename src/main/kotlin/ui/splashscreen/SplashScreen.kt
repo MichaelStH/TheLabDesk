01842be6f92caa7637a13be5fb032af9aea56887
MichaelStH <michael.sainthonore@gmail.com>
2023-11-09 13:03:30 +0100

    [develop]: animate progress bar's progress value in splashscreen

--- src/main/kotlin/ui/splashscreen/SplashScreen.kt
+++ src/main/kotlin/ui/splashscreen/SplashScreen.kt
@@ -1,15 +1,17 @@
 package ui.splashscreen
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -21,7 +23,9 @@ import core.compose.theme.TheLabDeskTheme
 import core.compose.theme.samsungSangFamily
 import di.AppModule
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import ui.main.MainViewModel
+import kotlin.random.Random
 
 //////////////////////////////////////////////////
 //
@@ -30,12 +34,18 @@ import ui.main.MainViewModel
 //////////////////////////////////////////////////
 @Composable
 fun SplashScreen(viewModel: MainViewModel) {
+    val scope = rememberCoroutineScope()
     var progress by remember { mutableFloatStateOf(0f) }
+
+    val animateProgress = animateFloatAsState(
+        targetValue = progress,
+        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
+    ).value
 
     TheLabDeskTheme {
         CardWithAnimatedBorder(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
             Column(
-                modifier = Modifier.fillMaxWidth(.8f).fillMaxHeight(.8f).background(color=Color.Transparent),
+                modifier = Modifier.fillMaxWidth(.8f).fillMaxHeight(.8f).background(color = Color.Transparent),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically)
             ) {
@@ -52,19 +62,21 @@ fun SplashScreen(viewModel: MainViewModel) {
                 )
                 LinearProgressIndicator(
                     modifier = Modifier.fillMaxWidth(.75f),
-                    progress = if (LocalInspectionMode.current) .45f else progress
+                    progress = animateProgress,
+                    strokeCap = StrokeCap.Round
                 )
             }
         }
     }
 
     LaunchedEffect(Unit) {
-        while (progress < 1f) {
-            progress += .01f
-            delay(25)
+        scope.launch {
+            while (progress < 1f) {
+                progress += .01f
+                delay(Random.nextLong(from = 25, until = 50))
+            }
+            viewModel.updateIsLoading(true)
         }
-
-        viewModel.updateIsLoading(true)
     }
 }
 
