f839dac066b5061d8e5cb2a7fb758ebbd85cb1de
MichaelStH <michael.sainthonore@gmail.com>
2026-01-23 17:27:12 +0100

    [develop]: chore update Config

---
+++ core/common/src/main/kotlin/com/riders/thelabdesk/core/common/data/local/model/Config.kt
@@ -0,0 +1,6 @@
+package com.riders.thelabdesk.core.common.data.local.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Config(val isDarkMode: Boolean)
--- src/main/kotlin/data/remote/dto/Config.kt
+++
@@ -1,6 +0,0 @@
-package data.remote.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Config(val isDarkMode: Boolean)
