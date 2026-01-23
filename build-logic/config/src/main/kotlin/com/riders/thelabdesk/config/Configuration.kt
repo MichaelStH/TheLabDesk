887fc8e8ac2af18b5f239ea356137d6e407c9e84
MichaelStH <michael.sainthonore@gmail.com>
2026-01-23 13:51:00 +0100

    [develop]: add build-logic's config module

---
+++ build-logic/config/src/main/kotlin/com/riders/thelabdesk/config/Configuration.kt
@@ -0,0 +1,30 @@
+package com.riders.thelabdesk.config
+
+object Configuration {
+    const val GROUP = "com.riders"
+
+    const val PACKAGE_NAME_BUILD_LOGIC = "$GROUP.thelabdesk.buildlogic"
+    const val PACKAGE_NAME = "$GROUP.thelabdesk"
+
+    val version = Version(versionMajor = 1, versionMinor = 0, versionPatch = 0, isSnapshot = true)
+}
+
+data class Version(
+    val versionMajor: Int,
+    val versionMinor: Int,
+    val versionPatch: Int,
+    val isSnapshot: Boolean
+) {
+    override fun toString(): String = StringBuilder().apply {
+        append(versionMajor)
+        append('.')
+        append(versionMinor)
+        append('.')
+        append(versionPatch)
+        if (isSnapshot) {
+            append('-')
+            append("SNAPSHOT")
+        }
+    }
+        .toString()
+}
