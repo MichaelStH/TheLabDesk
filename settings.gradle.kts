6746b3cf48defe427ffd5508b1a250ad81b8a0ac
MichaelStH <michael.sainthonore@gmail.com>
2026-01-23 13:52:00 +0100

    [develop]: chore add build-logic to project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,28 @@
 pluginManagement {
+
+    includeBuild("build-logic")
+
+
     repositories {
         gradlePluginPortal()
         google()
+
+        maven("https://jitpack.io")
+        maven("https://plugins.gradle.org/m2/")
+
         mavenCentral()
-        mavenLocal()
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
 
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         maven("https://packages.jetbrains.team/maven/p/firework/dev")
-        // temp
+        // Temp
         maven("https://packages.jetbrains.team/maven/p/ui/dev")
+        // Ktor Early Access Program Repository
+        maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
+        maven("https://androidx.dev/storage/compose-compiler/repository")
+
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
 
     plugins {
@@ -21,22 +33,40 @@ pluginManagement {
         id("org.jetbrains.kotlin.plugin.compose").version(extra["kotlin.version"] as String)
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+
+
+        maven("https://jitpack.io")
+        maven("https://plugins.gradle.org/m2/")
+
         mavenCentral()
         mavenLocal()
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
 
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         maven("https://packages.jetbrains.team/maven/p/firework/dev")
-
-        // temp
+        // Temp
         maven("https://packages.jetbrains.team/maven/p/ui/dev")
+        // Ktor Early Access Program Repository
+        maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
+        maven("https://androidx.dev/storage/compose-compiler/repository")
+
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
 rootProject.name = "LabDesk"
+include(":core:common")
+include(":core:data")
+include(":core:domain")
+include(":core:ui")
+include(":feature:home")
+include(":feature:splashscreen")
